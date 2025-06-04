7baa5a5ef213bb4fe49729a6257e535a70b09030
PATEL DHAVALKUMAR SANJAYBHAI <147937030+Dsp2810@users.noreply.github.com>
2025-06-04 11:28:47 +0530

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,4 +1,4 @@
 class Config:
-    SECRET_KEY = '4a2d79d4e64a4cb399f30e6d6fc2b7c9c30b6b3f3a3e5f9b8c4e8a3f3e8c6a2f'  # Replace with a strong key
-    MONGO_URI = "mongodb+srv://dspatel0006:Dsp2006@clu1.hlxvict.mongodb.net/student_management_system"
-    JWT_SECRET_KEY = '4a2d79d4e64a4cb399f30e6d6fc2b7c9c30b6b3f3a3e5f9b8c4e8a3f3e8c6a2f'  # Replace with a strong key
+    SECRET_KEY = ''  # Replace with a strong key
+    MONGO_URI = "" # Replace with your MongoDB URL
+    JWT_SECRET_KEY = ''  # Replace with a strong key
